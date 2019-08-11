c83fa6570b3032ce4291de45d01a013948d0a44d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-11 16:10:44 +0000

    Deploy ImpulseAdventure/GUIslice to github.com/ImpulseAdventure/GUIslice.git:gh-pages

---
+++ navtreedata.js
@@ -0,0 +1,45 @@
+var NAVTREE =
+[
+  [ "GUIslice", "index.html", [
+    [ "GUIslice library", "index.html", null ],
+    [ "Todo List", "todo.html", null ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", null ],
+        [ "Variables", "functions_vars.html", "functions_vars" ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", "globals_func" ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", "globals_defs" ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"GUIslice_8c.html",
+"GUIslice__drv__m5stack_8h.html#a17d2d9fe6cfdf908d8192fd6274155bf",
+"XGauge_8c.html#af7c7e483b8d6eaf9d98e1d912513f10a",
+"classTHPoint.html#ac40cb8454f0ce520f7e1136b989e8a0a",
+"group____GeneralFunc__.html#gadde52a7a89ddd0c4b65cd232ef073762",
+"group____IntPage__.html#ga0e887bb1d7fd8af195f689250d57ef29",
+"group____Int__.html#ga94c45de194bf195a1e653d5023628940",
+"group____Int__.html#gaf03910dc30dd3564494d2c339dc841e5",
+"structgslc__tsXTextbox.html#ae2b2161ce56204973ab63536f52ac0a1"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ navtreeindex8.js
@@ -0,0 +1,6 @@
+var NAVTREEINDEX8 =
+{
+"structgslc__tsXTextbox.html#ae2b2161ce56204973ab63536f52ac0a1":[3,0,24,15],
+"structgslc__tsXTextbox.html#af205dba9c3902ed6df3fc0191bf79037":[3,0,24,6],
+"todo.html":[1]
+};
