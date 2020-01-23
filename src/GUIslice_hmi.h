a9d41ed88d4792d052402c512b7759ebad3cb0af
David Hijlkema <David.Hijlkema@gmail.com>
2020-01-23 21:44:46 +0100

    Fixes and start of HMI parsing in the sketch to test the code

--- src/GUIslice_hmi.h
+++ src/GUIslice_hmi.h
@@ -3,7 +3,15 @@
 #ifndef _GUISLICE_HMI_h
 #define _GUISLICE_HMI_h
 
+#include <Arduino.h>
 #include "GUIslice.h"
+
+
+// uint8_t buffer[HMI_SERIAL_BUFFER_SIZE]; // User configurable so as not to strain memory
+// uint16_t currentBufferIndex = 0; // Used to hold the index of the buffer
+
+#define HMI_BUFFER_ERROR (uint8_t[]){0xA0,0xFF,0xFF,0xFF}
+
 
 void gslc_hmi_sendTouchDown(void* pvGui, void* pvElemRef);
 void gslc_hmi_sendTouchUp(void* pvGui, void* pvElemRef);
@@ -11,5 +19,5 @@ void gslc_hmi_sendTouchMove(void* pvGui, void* pvElemRef);
 void gslc_hmi_sendPageLoaded(void* pvGui, int16_t pageId);
 void gslc_hmi_sendPageUnloaded(void* pvGui, int16_t pageId);
 void gslc_hmi_sendValueChanged(void* pvGui, void* pvElemRef);
-
+void gslc_hmi_loop(void* pvGui);
 #endif
